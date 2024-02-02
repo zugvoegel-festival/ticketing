efce324da8075a6082b7ef4fa1bf63d1f9ca3949
Manuel.Huettel <manuel.huettel@invers.com>
2024-02-02 09:05:41 +0100

    cleanup and resolved review

--- modules/services/backup/restic.nix
+++ modules/services/backup/restic.nix
@@ -1,39 +1,44 @@
 { config, lib, pkgs, ... }:
-
 with lib;
-
+let
+  cfg = config.zugvoegel.services.backup;
+in
 {
-  options = {
-    backup = mkOption {
-      type = types.listOf types.string;
+  options.zugvoegel.services.backup.restic = {
+    # Define option to enable the pretix config
+    enable = mkEnableOption "Pretix ticketing service";
+
+    # Define options for restic
+    backupDir = mkOption {
+      type = types.listOf types.str;
       default = [ ];
       example = [ "/path/to/backup/directory" ];
       description = "Directories to be backed up using restic";
     };
 
     resticPasswordFile = mkOption {
-      type = types.string;
-      default = "";
+      type = types.str;
+      default = null;
       example = "/etc/restic/password.txt";
       description = "Path to the file containing the restic repository password";
     };
 
     onedriveConfigFile = mkOption {
-      type = types.string;
-      default = "";
+      type = types.str;
+      default = null;
       example = "/etc/restic/onedrive.conf";
       description = "Path to the rclone configuration file for OneDrive";
     };
 
     onedrivePath = mkOption {
-      type = types.string;
-      default = "";
+      type = types.str;
+      default = null;
       example = "/my/onedrive/destination";
       description = "Path in your OneDrive";
     };
   };
 
-  config = {
+  config = mkIf cfg.enable {
     services = {
       resticBackup = {
         enable = true;
