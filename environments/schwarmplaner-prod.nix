8bdff2325f480aa331a0931a2035c0ab628143f4
Manuel Hüttel <manuel.huettel@MBPMHuettel.local>
2026-05-22 17:48:08 +0200

    chore(deploy): pin schwarmplaner prod to 0.1.6

--- environments/schwarmplaner-prod.nix
+++ environments/schwarmplaner-prod.nix
@@ -3,7 +3,7 @@
   # Schwarmplaner production — app-image bumped by schwarmplaner release-prod.sh
   zugvoegel.services.schwarmplaner.instances.prod = {
     host = "schwarmplaner.zugvoegelfestival.org";
-    app-image = "manulinger/schwarmplaner:0.1.5";
+    app-image = "manulinger/schwarmplaner:0.1.6";
     acmeMail = "webmaster@zugvoegelfestival.org";
     port = 3303;
   };
