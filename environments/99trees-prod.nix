5f26429948c2e93b92d47e6e1f77f8ac24c7c85b
Manuel Hüttel <manuel.huettel@MBPMHuettel.local>
2026-05-22 16:44:17 +0200

    chore(deploy): pin 99trees prod to 1.0.3

--- environments/99trees-prod.nix
+++ environments/99trees-prod.nix
@@ -3,7 +3,7 @@
   # 99trees production only — app-image bumped by 99trees release-prod.sh
   zugvoegel.services.trees99.instances.prod = {
     host = "trees.loco.vision";
-    app-image = "manulinger/99trees:1.0.2";
+    app-image = "manulinger/99trees:1.0.3";
     acmeMail = "webmaster@zugvoegelfestival.org";
     port = 3323;
   };
